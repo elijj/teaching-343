b0e7b819abec8484dce23c6234fe10e4e17648b6
Kirk Lestelle <kirklest@uw.edu>
2015-12-07 08:55:22 -0800

    fixed sprite loading error

--- app.js
+++ app.js
@@ -4,11 +4,7 @@ var game = new Phaser.Game(640, 480, Phaser.CANVAS, 'game');
 
         this.player = null;
         this.platforms = null;
-
-        this.facing = 'left';
-        this.edgeTimer = 0;
         this.jumpTimer = 0;
-
         this.wasStanding = false;
         this.cursors = null;
 
@@ -56,7 +52,7 @@ var game = new Phaser.Game(640, 480, Phaser.CANVAS, 'game');
                 var type = i % 2 === 1 ? 'platform' : 'ice-platform';
                 var platform = this.platforms.create(x, y, type);
 
-                //  Set a random speed between 50 and 200
+                //  Set a random speed between 100 and 150
                 platform.body.velocity.x = this.rnd.between(100, 150);
 
                 //  Inverse it?
@@ -116,9 +112,10 @@ var game = new Phaser.Game(640, 480, Phaser.CANVAS, 'game');
 
             this.platforms.forEach(this.wrapPlatform, this);
 
+            // collision check 
             this.physics.arcade.collide(this.player, this.platforms, this.setFriction, null, this);
 
-            //  Do this AFTER the collide check, or we won't have blocked/touching set
+            //  do this AFTER the collide check, or we won't have blocked/touching set
             var standing = this.player.body.blocked.down || this.player.body.touching.down;
 
             this.player.body.velocity.x = 0;
@@ -127,55 +124,20 @@ var game = new Phaser.Game(640, 480, Phaser.CANVAS, 'game');
             {
                 this.player.body.velocity.x = -200;
 
-                if (this.facing !== 'left')
-                {
-                    this.player.play('left');
-                    this.facing = 'left';
-                }
             }
             else if (this.cursors.right.isDown)
             {
                 this.player.body.velocity.x = 200;
-
-                if (this.facing !== 'right')
-                {
-                    this.player.play('right');
-                    this.facing = 'right';
-                }
             }
-            else
-            {
-                if (this.facing !== 'idle')
-                {
-                    // this.player.animations.stop();
-
-                    if (this.facing === 'left')
-                    {
-                        this.player.frame = 0;
-                    }
-                    else
-                    {
-                        this.player.frame = 5;
-                    }
-
-                    this.facing = 'idle';
-                }
-            }
-
-            //  No longer standing on the edge, but were
-            //  Give them a 250ms grace period to jump after falling
-            if (!standing && this.wasStanding)
-            {
-                this.nedgeTimer = this.time.time + 250;
-            }
-
+           
             //  Allowed to jump?
-            if ((standing || this.time.time <= this.edgeTimer) && this.cursors.up.isDown && this.time.time > this.jumpTimer)
+            if ((standing) && this.cursors.up.isDown && this.time.time > this.jumpTimer)
             {
                 this.player.body.velocity.y = -500;
                 this.jumpTimer = this.time.time + 750;
             }
 
+            // reset standing boolean
             this.wasStanding = standing;
 
         }
